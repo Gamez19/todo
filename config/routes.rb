5ed1c957be9517e3d878251a52f48cc30ac20072
Gamez19 <edwingamez19@gmail.com>
2018-09-18 15:16:56 -0600

    Se añade la opcion de crear listas de tareas, items a las listas y eliminar los items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
- # get 'welcome/index'
-  resources :articles
+ 
+  resources :todo_lists do
+  	resources :todo_items
+  end	
 
-  root 'welcome#index'
+  root 'todo_lists#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'welcome/index'
+  #resources :articles
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_17_175624) do
+ActiveRecord::Schema.define(version: 2018_09_18_194529) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "todo_items", force: :cascade do |t|
+    t.string "content"
+    t.integer "todo_list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+  end
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
