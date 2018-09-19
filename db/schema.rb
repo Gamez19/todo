ac171b6fba496f9d6a7ca4770769decdbff3aa8c
Gamez19 <edwingamez19@gmail.com>
2018-09-19 00:18:46 -0600

    añadio la habilidad de marcar completado los items con poco de estilos a los completados

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_18_194529) do
+ActiveRecord::Schema.define(version: 2018_09_19_053246) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_09_18_194529) do
     t.integer "todo_list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
